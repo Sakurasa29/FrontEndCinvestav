94fa7cf7d7e1dbfe5a758753489968f772e0e9e0
Sarah Lo <sarahlopezch@icloud.com>
2018-03-05 11:12:52 -0600

    Funcionalidad de menu y menu

---
+++ app/components/settings/main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Settings extends React.Component {
+    render() {
+        return (
+            <div className="settings">Yo soy el settings
+            </div>
+        );
+    }
+}
+
+export default Settings;
